71830164e1937650aebe5e4fc6c0e0b10b61037d
Ivan Gufler <ivan.gufler@tfobz.net>
2020-12-19 23:07:16 +0100

    defined all basic api views and methods

--- elections/urls.py
+++ elections/urls.py
@@ -3,6 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('vote/<str:token>', views.ElectionList.as_view()),
     path('election', views.ElectionList.as_view()),
     path('election/<int:election_id>', views.ElectionDetail.as_view()),
+    path('election/<int:election_id>/start', views.StartElection.as_view()),
+    path('election/<int:election_id>/end', views.EndElection.as_view()),
+    path('election/<int:election_id>/pause', views.PauseElection.as_view()),
+    path('election/<int:election_id>/remind', views.VoteReminder.as_view()),
+    path('election/<int:election_id>/voter', views.VoterList.as_view()),
+    path('election/<int:election_id>/voter/<str:email>', views.VoterDetail.as_view()),
+    path('election/<int:election_id>/option', views.OptionList.as_view()),
+    path('election/<int:election_id>/option/<int:index>', views.OptionDetail.as_view()),
 ]
--- elections/views.py
+++ elections/views.py
@@ -3,9 +3,70 @@ from rest_framework.views import APIView
 
 
 # Create your views here.
-class ElectionList(APIView):
-    pass
+class ElectionAPI(APIView):
+    def get_election(self, election_id):
+        pass
 
 
-class ElectionDetail(APIView):
-    pass
+class VoteView(ElectionAPI):
+    def get(self, request, token):
+        pass
+
+    def post(self, request, token):
+        pass
+
+
+class ElectionList(ElectionAPI):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ElectionDetail(ElectionAPI):
+    def get(self, request, election_id):
+        pass
+
+    def put(self, request, election_id):
+        pass
+
+
+class StartElection(ElectionAPI):
+    def post(self, request, election_id):
+        pass
+
+
+class EndElection(ElectionAPI):
+    def post(self, request, election_id):
+        pass
+
+
+class PauseElection(ElectionAPI):
+    def post(self, request, election_id):
+        pass
+
+
+class VoteReminder(ElectionAPI):
+    def post(self, request, election_id):
+        pass
+
+
+class OptionList(ElectionAPI):
+    def post(self, request, election_id):
+        pass
+
+
+class OptionDetail(ElectionAPI):
+    def put(self, request, election_id, index):
+        pass
+
+
+class VoterList(ElectionAPI):
+    def post(self, request, election_id):
+        pass
+
+
+class VoterDetail(ElectionAPI):
+    def delete(self, request, election_id, email):
+        pass
