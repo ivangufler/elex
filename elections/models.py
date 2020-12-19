5b43e27cd7aae2b42f0e106eb91dc3f8f5715bf4
Ivan Gufler <ivan.gufler@tfobz.net>
2020-12-19 22:12:30 +0100

    created and migrated models

--- elections/models.py
+++ elections/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Election(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=500, blank=True, default='')
+    admin = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING
+    )
+    paused = models.BooleanField(default=False)
+    votable = models.IntegerField(default=1)
+    voters = models.IntegerField(default=0)
+    voted = models.IntegerField(default=0)
+    creation_date = models.DateTimeField(default=timezone.now)
+    start_date = models.DateTimeField(null=True, default=None)
+    end_date = models.DateTimeField(null=True, default=None)
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    election = models.ForeignKey(
+        Election, on_delete=models.CASCADE
+    )
+    votes = models.IntegerField(default=0)
+
+    class Meta:
+        managed = False
+        db_table = 'elections_option'
+
+
+class Voter(models.Model):
+    token = models.CharField(unique=True, max_length=255)
+    email = models.EmailField(max_length=255, primary_key=True)
+    election = models.ForeignKey(
+        Election, on_delete=models.CASCADE
+    )
+    voted = models.BooleanField(default=False)
+
+    class Meta:
+        managed = False
+        db_table = 'elections_voter'
