355a6fe530af6adb6bee258f0c7d6e460c3c2081
Ivan Gufler <ivan.gufler@tfobz.net>
2021-04-26 10:16:23 +0200

    Created config interface

---
+++ elex/config.py
@@ -0,0 +1,21 @@
+# insert random generated secret key
+SECRET_KEY = ''
+
+# insert keys for oauth2 over microsoft azure ad
+OAUTH2_KEY = ''
+OAUTH2_SECRET = ''
+OAUTH2_TENANT = ''
+
+# redirect urls after logging in/off
+LOGIN_REDIRECT = '/election'
+LOGOUT_REDIRECT = '/'
+
+# configs for sending emails
+FROM_EMAIL = 'Elex – Digitale Wahlen <test@example.com>'
+EMAIL_HOST = 'mail.example.com'
+EMAIL_PORT = 0
+EMAIL_USER = 'user@example.com'
+EMAIL_PASSWORD = '123456'
+
+HOSTNAME = 'http://localhost'
+DEVELOPMENT = False
